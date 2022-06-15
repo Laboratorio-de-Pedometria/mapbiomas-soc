96fe0a92ec18be86643712d8bdbb33a383634150
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2022-06-15 15:05:30 -0300

    latest version

--- src/02-carbon-stock.r
+++ src/02-carbon-stock.r
@@ -16,11 +16,26 @@ febr_data <- data.table::fread("mapbiomas-solos/data/febr-data.txt", dec = ",", 
 colnames(febr_data)
 
 # Compute carbon stock per layer
-febr_data[, estoque := carbono * (espessura / 100) + dsi * (terrafina / 1000)]
-hist(febr_data[, estoque])
+
+febr_data[, estoque := round(carbono * (espessura / 100) + dsi * (terrafina / 1000), 2)]
+febr_data[, log_estoque := log1p(estoque)]
+hist(febr_data[, log_estoque])
 
 # Create spatial object
+topsoil_idx <- febr_data[["profund_sup"]] == 0
+has_stock_idx <- !is.na(febr_data[["estoque"]])
+has_coord_idx <- (!is.na(febr_data[["coord_x"]]) + !is.na(febr_data[["coord_y"]]) == 2)
 sf_idx <- !is.na(febr_data[, coord_x]) & !is.na(febr_data[, coord_y])
-febr_data_sf <- sf::st_as_sf(febr_data[sf_idx, ], coords = c("coord_x", "coord_y"), crs = 4623)
-topsoil_idx <- febr_data_sf[["profund_sup"]] == 0
-plot(febr_data_sf[topsoil_idx, "estoque"], pch = 20, cex = 0.5)
+topsoil_has_stock_coord_idx <- which((topsoil_idx + has_stock_idx + sf_idx) == 3)
+
+# Write object to disk
+colnames(febr_data)
+vars_idx <- c("dataset_id", "observacao_id", "camada_id", "coord_x", "coord_y", "estoque")
+febr_data[, coord_x := round(as.numeric(coord_x), 8)]
+febr_data[, coord_y := round(as.numeric(coord_y), 8)]
+  write.table(febr_data[topsoil_has_stock_coord_idx, ..vars_idx],
+    file = "mapbiomas-solos/res/pontos-estoque.csv",
+    row.names = FALSE, sep = ",", dec = ".")
+
+
+
