919ba866af78cf38d63d7f2c7a7f349a790ca704
samuel-rosa <alessandrosamuelrosa@gmail.com>
2023-03-08 10:47:37 -0300

    sample gee

--- src/04-prepare-environmental-covariates.r
+++ src/04-prepare-environmental-covariates.r
@@ -10,6 +10,9 @@ if (!require("sf")) {
 if (!require("rgee")) {
   install.packages("rgee", dependencies = TRUE)
 }
+if (!require("geobr")) {
+  install.packages("geobr")
+}
 
 # Initialize Google Earth Engine
 rgee::ee_Initialize()
@@ -22,16 +25,35 @@ colnames(febr_data)
 # First filter out those samples without coordinates
 # Also keep a single sample per soil profile
 is_na_coordinates <- is.na(febr_data[, coord_x]) | is.na(febr_data[, coord_y])
-sf_febr_data <- febr_data[!is_na_coordinates,
+sp_febr_data <- febr_data[!is_na_coordinates, ]
+sf_febr_data <- sp_febr_data[,
   data.frame(coord_x = mean(coord_x), coord_y = mean(coord_y)),
   by = c("dataset_id", "id")
 ]
 sf_febr_data <- sf::st_as_sf(sf_febr_data, coords = c("coord_x", "coord_y"), crs = 4326)
+brazil <- geobr::read_country()
+x11()
+plot(brazil, reset = FALSE)
+plot(sf_febr_data, add = TRUE)
+
+# Prepare for sampling on GEE
+n_points <- nrow(sf_febr_data)
+n_lags <- ceiling(n_points / 5000)
+lag_width <- ceiling(n_points / n_lags)
+lags <- rep(1:n_lags, each = lag_width)
+lags <- lags[1:n_points]
 
 # Soil Grids 250m v2.0
-bdod_mean <- rgee::ee_extract(
-  x = ee$Image("projects/soilgrids-isric/bdod_mean"),
-  y = sf_febr_data[1:5000, ], scale = 250
-)
-# sf_febr_data <- cbind(sf_febr_data, bdod_mean)
+bdod_mean <- list()
+for (i in 1:n_lags) {
+  bdod_mean[[i]] <- rgee::ee_extract(
+    x = ee$Image("projects/soilgrids-isric/bdod_mean"),
+    y = sf_febr_data[lags == i, ], scale = 250, quiet = FALSE
+  )
+}
+bdod_mean <- data.table::rbindlist(bdod_mean)
+sf_febr_data <- cbind(sf_febr_data, bdod_mean[, 4:ncol(bdod_mean)])
+sf_febr_data
+
+tmp <- merge(sf_febr_data, sp_febr_data, by = c("dataset_id", "id"))
 
