9f1b7c0a30e3e0c6c808cf1cad4ca0a3d49d2444
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2023-03-09 18:23:46 -0300

    use ranger instead of randomForest

--- src/05-estimate-soil-bd.r
+++ src/05-estimate-soil-bd.r
@@ -1,41 +1,41 @@
 rm(list = ls())
 
 # Install and load required packages
-# if (!require("partykit")) {
-#   install.packages("partykit")
-# }
-if (!require("randomForest")) {
-  install.packages("randomForest")
-}
-if (!require("pedometrics")) {
-  install.packages("pedometrics")
+if (!require("ranger")) {
+  install.packages("ranger")
 }
 
 # Ler dados do disco
-febr_data <- data.table::fread("mapbiomas-solos/data/01-febr-data.txt", dec = ",", sep = "\t")
+febr_data <- data.table::fread("mapbiomas-solos/data/04-febr-data.txt", dec = ",", sep = "\t")
 colnames(febr_data)
 
-# Compute carbon related covariates
-febr_data[, log_carbono := log1p(carbono)]
-febr_data[, argila_carbono := argila * carbono]
-summary(febr_data[, c("log_carbono", "argila_carbono")])
+# We work with data from the first 30 cm and ignore organic layers
+febr_data <- febr_data[profund_sup >= 0 & profund_sup < 30, ]
+nrow(febr_data)
 
-# Identify layers missing soil density data
-dsi_notna_idx <- !is.na(febr_data[["dsi"]])
-hist(febr_data[dsi_notna_idx, dsi],
+# Compute carbon related covariates
+# febr_data[, log_carbono := log1p(carbono)]
+# febr_data[, argila_carbono := argila * carbono]
+# summary(febr_data[, c("log_carbono", "argila_carbono")])
+
+# Identify layers not missing soil density data
+dsi_isna <- is.na(febr_data[["dsi"]])
+sum(!dsi_isna); sum(dsi_isna)
+x11()
+hist(febr_data[!dsi_isna, dsi],
   xlab = expression("Densidade, g cm"^-3), ylab = "Frequência", main = "")
-rug(febr_data[dsi_notna_idx, dsi])
+rug(febr_data[!dsi_isna, dsi])
 
 # Exploratory data analysis
-r_vars <- c(
-  "argila", "areia", "carbono", "log_carbono", "argila_carbono", "silte", "profund", "terrafina")
-r_matrix <- cor(febr_data[dsi_notna_idx, ..r_vars], use = "complete", method = "spearman")
-pedometrics::plotCor(round(r_matrix, 2))
+# r_vars <- c(
+#   "argila", "areia", "carbono", "log_carbono", "argila_carbono", "silte", "profund", "terrafina")
+# r_matrix <- cor(febr_data[dsi_notna_idx, ..r_vars], use = "complete", method = "spearman")
+# pedometrics::plotCor(round(r_matrix, 2))
 
 # Set categorical variables as factors
-febr_data[, estado_id := as.factor(estado_id)]
-state_table <- sort(table(febr_data[dsi_notna_idx, estado_id]), decreasing = TRUE)
-barplot(state_table, xlab = "Unidade da federação", ylab = "Frequência")
+# febr_data[, estado_id := as.factor(estado_id)]
+# state_table <- sort(table(febr_data[dsi_notna_idx, estado_id]), decreasing = TRUE)
+# barplot(state_table, xlab = "Unidade da federação", ylab = "Frequência")
 
 # Imputar valores faltantes
 # colnames(febr_data)
@@ -50,50 +50,55 @@ barplot(state_table, xlab = "Unidade da federação", ylab = "Frequência")
 # febr_data[, coord_y := randomForest::na.roughfix(coord_y)]
 
 # Estimate random forest model
-dsi_formula <- dsi ~
-  terrafina +
-  # silte +
-  areia + argila +
-  # ctc + ph +
-  carbono + argila_carbono +
-  # log_carbono +
-  profund +
-  estado_id
-  # sibcs_ordem +
-  # sibcs_subordem +
-  # taxon_sibcs +
-  # coord_x + coord_y
+colnames(febr_data)
+febr_data[, psd := NULL]
+febr_data[, GREATGROUP := NULL]
+febr_data[, SUBGROUP := NULL]
+covars <- colnames(febr_data)
+idx <- which(covars == "ORDER")
+covars <- covars[idx:length(covars)]
+dsi_formula <- paste0("dsi ~ ", paste0(covars, collapse = " + "))
+
 t0 <- proc.time()
-dsi_model <- randomForest::randomForest(
+dsi_model <- ranger::ranger(
   formula = dsi_formula,
-  data = febr_data[dsi_notna_idx, ],
-  na.action = randomForest::na.roughfix,
-  ntree = 500,
-  importance = TRUE
+  data = febr_data[!dsi_isna, ],
+  num.trees = 500,
+  importance = "impurity"
 )
 proc.time() - t0
 
 # Compute model statistics
 print(dsi_model)
-plot(dsi_model, panel.first = {
-  grid()
-})
-dsi_model_imp <- randomForest::importance(dsi_model)
-dsi_model_imp <- dsi_model_imp[order(dsi_model_imp[, 1]), ]
-par(mar = c(4, 7, 4, 4))
-barplot(dsi_model_imp[, 1], horiz = TRUE, las = 1)
+
+# Variable importance
+x11()
+par(mar = c(5, 10, 4, 2) + 0.1)
+barplot(sort(dsi_model$variable.importance), horiz = TRUE, las = 1)
+
+# Predicted versus observed
+x11()
+plot(y = febr_data[!dsi_isna, dsi], x = dsi_model$predictions, xlim = c(0, 3), ylim = c(0, 3), 
+  panel.first = grid()
+)
+abline(0, 1)
+
+# OOB RMSE
+round(sqrt(dsi_model$prediction.error), 2)
+
+# dsi_model_imp <- randomForest::importance(dsi_model)
+# dsi_model_imp <- dsi_model_imp[order(dsi_model_imp[, 1]), ]
+# par(mar = c(4, 7, 4, 4))
+# barplot(dsi_model_imp[, 1], horiz = TRUE, las = 1)
 
 # Predict soil bulk density
-febr_data[!dsi_notna_idx, "dsi"] <- predict(
-  dsi_model, newdata = febr_data[!dsi_notna_idx, ])
-febr_data[, dsi := round(dsi, 2)]
-hist(febr_data[["dsi"]])
-rug(febr_data[["dsi"]])
+tmp <- predict(dsi_model, data = febr_data[dsi_isna, ])
+febr_data[dsi_isna, dsi := round(tmp$predictions, 2)]
 
 # Escrever dados em disco
 data.table::fwrite(
   febr_data[!is.na(carbono) | !is.na(dsi), ],
-  "mapbiomas-solos/data/02-febr-data.txt", sep = "\t", dec = ",")
+  "mapbiomas-solos/data/05-febr-data.txt", sep = "\t", dec = ",")
 
 # Estimate recursive partitioning regression model #################################################
 # dsi_formula <- dsi ~ argila + silte + carbono + log1p(carbono) + I(argila * carbono) | profund + terrafina + areia
