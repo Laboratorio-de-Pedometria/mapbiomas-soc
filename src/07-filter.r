a7b3cf8e0eadddd9a4ed5d0e46f79c78f021b417
samuel-rosa <alessandrosamuelrosa@gmail.com>
2023-03-13 17:38:15 -0300

    filter data

--- src/07-filter.r
+++ src/07-filter.r
@@ -12,7 +12,6 @@ if (!require("geobr")) {
 }
 if (!require("rgee")) {
   install.packages("rgee", dependencies = TRUE)
-  rgee::ee_install()
 }
 rgee::ee_Initialize()
 
@@ -20,15 +19,20 @@ rgee::ee_Initialize()
 febr_data <- data.table::fread("mapbiomas-solos/data/06-febr-data.txt", dec = ",", sep = "\t")
 colnames(febr_data)
 
+febr_data[data_coleta_ano < 1985, data_coleta_ano := 1985]
+
 # Create spatial object
-sf_febr_data <- sf::st_as_sf(febr_data, coords = c("coord_x", "coord_y"), crs = 4326)
+febr_data <- sf::st_as_sf(febr_data, coords = c("coord_x", "coord_y"), crs = 4326)
 brazil <- geobr::read_country()
 x11()
 plot(brazil, reset = FALSE)
-plot(sf_febr_data, add = TRUE, col = "black", cex = 0.5)
+plot(febr_data, add = TRUE, col = "black", cex = 0.5)
+
+
+
 
 # Prepare for sampling on GEE
-n_points <- nrow(sf_febr_data)
+n_points <- nrow(febr_data)
 n_lags <- ceiling(n_points / 5000)
 lag_width <- ceiling(n_points / n_lags)
 lags <- rep(1:n_lags, each = lag_width)
@@ -37,12 +41,25 @@ lags <- lags[1:n_points]
 
 # MapBiomas Land Cover Land Use
 mapbiomas <- list()
-x <- rgee::ee$ImageCollection("projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2")
-for (i in 1:n_lags) {
-  bdod_mean[[i]] <- rgee::ee_extract(
-    x = rgee::ee$ImageCollection$filterDate(x, paste0(year, "-01-01"), paste0(year, "-12-31")),
-    y = sf_febr_data[lags == i, ],
+time_steps <- sort(unique(febr_data[["data_coleta_ano"]]))
+gee_path <- "projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2"
+gee_path <- rgee::ee$Image(gee_path)
+for (year in time_steps) {
+  idx <- febr_data[["data_coleta_ano"]] == year
+  mapbiomas[[year]] <- rgee::ee_extract(
+    x = rgee::ee$Image$filterDate(gee_path,
+      paste0(year, "-01-01"), paste0(year, "-12-31")),
+    y = febr_data[idx, ],
     scale = 30,
     fun = ee$Reducer$first()
   )
 }
+
+
+
+
+# Escrever dados de estoque de carbono no solo em disco
+febr_data[, coord_x := round(as.numeric(coord_x), 8)]
+febr_data[, coord_y := round(as.numeric(coord_y), 8)]
+write.table(febr_data, file = paste0("mapbiomas-solos/res/pontos-estoque.csv"),
+  row.names = FALSE, sep = ",", dec = ".")
