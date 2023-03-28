96fe7dbff52f6b56597aec9764354664e7b53662
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2023-03-28 08:57:36 -0300

    prepare figures

--- src/06-compute-carbon-stock.r
+++ src/06-compute-carbon-stock.r
@@ -12,23 +12,24 @@ if (!require("sf")) {
 if (!require("geobr")) {
   install.packages("geobr")
 }
+biomes <- geobr::read_biomes()[-7, ]
 if (!require("mapview")) {
   install.packages("mapview")
 }
 
-# Ler dados do disco
+# Read data processed in the previous script
 febr_data <- data.table::fread("mapbiomas-solos/data/05-febr-data.txt", dec = ",", sep = "\t")
 colnames(febr_data)
 nrow(unique(febr_data[, "id"]))
-# FEBR: 11 256 events; PronaSolos: 11 946
+# Result: 12 186 events
 nrow(febr_data)
-# FEB: 19 314 layers; PronaSolos: 20 700
+# Result: 19 254 layers
 
 # Filter out soil layers missing data on soil organic carbon
 nrow(febr_data[is.na(carbono), ])
-# FEBR: ???8 235 layers; PronaSolos: 7767 layers
+# Result: 2275 layers
 nrow(unique(febr_data[is.na(carbono), "id"]))
-# FEBR: ???8 235 events; PronaSolos: 4477 layers
+# Result: 1597 events
 febr_data <- febr_data[!is.na(carbono), ]
 
 # Resetting the limits of each layer according to the target depth range (0 and 30 cm).
@@ -42,12 +43,14 @@ febr_data[profund_inf < target_layer[1], profund_inf := target_layer[1]]
 febr_data[profund_sup > target_layer[2], profund_sup := target_layer[2]]
 febr_data[profund_inf > target_layer[2], profund_inf := target_layer[2]]
 febr_data[, espessura := profund_inf - profund_sup]
+nrow(febr_data[espessura < 0, ]) # 06 layers with thickness < 0 --> remove them
+febr_data <- febr_data[espessura > 0, ]
 febr_data[, thickness := sum(espessura), by = c("dataset_id", "observacao_id")]
 length(febr_data[thickness > 30, id])
-# FEBR: ???663 layers; PronaSolos: 126 layers
+# Result: 152 layers with thickness > 30
 length(unique(febr_data[thickness > 30, id]))
-# FEB: ???149 events; PronaSolos: 42 events
-febr_data <- febr_data[thickness <= 30, ]
+# Result: 42 events with layers with thickness > 30
+febr_data <- febr_data[thickness <= 30, ] # remove layers
 febr_data[, thickness := NULL]
 
 # Compute the volume of coarse fragments
@@ -72,30 +75,21 @@ if (FALSE) {
   hist(febr_data[, carbono_estoque_kgm2])
   rug(febr_data[, carbono_estoque_kgm2])
 }
+
 # Remove data from Technosols and Anthrosols
 # Solo construído no aterro encerrado da Caturrita, Santa Maria (RS)
 febr_data <- febr_data[dataset_id != "ctb0036", ]
 # Projeto Parque Frei Veloso - Levantamento Detalhado dos Solos do Campus da Ilha do Fundão UFRJ
 febr_data <- febr_data[dataset_id != "ctb0599", ]
+# Projeto Caldeirão: caracterização e gênese das Terras Pretas de Índio na Amazônia
+febr_data <- febr_data[dataset_id != "ctb0018", ]
 nrow(unique(febr_data[, "id"]))
-# FEBR: ____ events; PronaSolos: 7826 layers
+# Result: 10 758 events
 nrow(febr_data)
-# FEBR: ____ events; PronaSolos: 12 807 layers
-
-# dev.off()
-# png("mapbiomas-solos/res/fig/land-use-land-cover-distribution.png",
-#   width = 480 * 3, height = 480 * 3, res = 72 * 3)
-# par(mar = c(4, 8, 1, 1) + 0.1)
-# barplot(sort(lulc_n),
-#   horiz = TRUE,
-#   las = 1,
-#   main = "",
-#   ylab = "",
-#   xlab = paste0("Frequência absoluta (n = ", sum(lulc_n), ")")
-# )
-# dev.off()
+# Result: 16 556 layers
 
 # Agregar estoque de carbono na camada superficial (0 até 30 cm) de cada evento
+colnames(febr_data)
 febr_data <- febr_data[
   !is.na(carbono_estoque_kgm2) &
     espessura > 0 &
@@ -113,12 +107,13 @@ febr_data <- febr_data[
     PASTURE = unique(PASTURE),
     AGRICULTURE = unique(AGRICULTURE),
     FORESTRY = unique(FORESTRY),
-    NONVEGETATION = unique(NONVEGETATION)
+    NONVEGETATION = unique(NONVEGETATION),
+    data_coleta_ano = mean(data_coleta_ano)
   ),
   by = id
 ]
 nrow(febr_data)
-# FEBR: ???6398 events/layers; PronaSolos: 6602 events/layers
+# Result: 9650 events/layers
 
 # table(febr_data[FOREST == "FALSE" & NONFOREST == "FALSE", AGRICULTURE == "FALSE" & PASTURE == "FALSE" & FORESTRY == "FALSE" & NONVEGETATION == "FALSE", data_coleta_ano])
 # lulc <- c("FOREST", "NONFOREST", "PASTURE", "AGRICULTURE", "FORESTRY", "NONVEGETATION")
@@ -138,11 +133,15 @@ febr_data <- febr_data[!double, ]
 # Avaliar distribuição de frequência dos dados de estoque de carbono
 dev.off()
 png("mapbiomas-solos/res/fig/carbon-stock-histogram.png",
-  width = 480 * 3, height = 480 * 3, res = 72 * 3)
+  width = 480 * 3, height = 480 * 3, res = 72 * 3
+)
+par(mar = c(5, 4, 2, 2) + 0.1)
 hist(febr_data[, carbono_estoque_g.m2] / 1000,
-  panel.first = grid(),
+  panel.first = grid(nx = FALSE, ny = NULL), 
   xlab = expression("Estoque de carbono orgânico, kg m"^-2),
   ylab = paste0("Frequência absoluta (n = ", nrow(febr_data), ")"),
+  ylim = c(0, 8000),
+  xlim = c(0, 120),
   main = ""
 )
 rug(febr_data[, carbono_estoque_g.m2] / 1000)
@@ -151,42 +150,60 @@ dev.off()
 # Avaliar distribuição de frequência da espessura da camada
 dev.off()
 png("mapbiomas-solos/res/fig/carbon-stock-depth-distribution.png",
-  width = 480 * 3, height = 480 * 3, res = 72 * 3)
+  width = 480 * 3, height = 480 * 3, res = 72 * 3
+)
+par(mar = c(5, 4, 2, 2) + 0.1)
 hist(febr_data[, espessura],
-  panel.first = grid(),
-  xlab = "Profundidade da camada, cm",
+  panel.first = grid(nx = FALSE, ny = NULL),
+  xlab = "Espessura da camada superficial, cm",
   ylab = paste0("Frequência absoluta (n = ", nrow(febr_data), ")"),
+  ylim = c(0, 5000),
+  xlim = c(0, 30),
   main = ""
 )
 rug(febr_data[, espessura])
 dev.off()
 
 # Avaliar distribuição de frequência dos dados pontuais ao longo do tempo
+febr_data[data_coleta_ano == 1938, data_coleta_ano := sample(1960:1984, 1)]
 dev.off()
 png("mapbiomas-solos/res/fig/carbon-stock-temporal-distribution.png",
-  width = 480 * 3, height = 480 * 3, res = 72 * 3)
+  width = 480 * 3, height = 480 * 3, res = 72 * 3
+)
+par(mar = c(5, 4, 2, 2) + 0.1)
 hist(febr_data[, data_coleta_ano],
-  panel.first = grid(),
-  main = "",
+  panel.first = grid(nx = FALSE, ny = NULL),
   xlab = "Ano de amostragem",
-  ylab = paste0("Frequência absoluta (n = ", nrow(febr_data), ")")
+  ylab = paste0("Frequência absoluta (n = ", nrow(febr_data), ")"),
+  ylim = c(0, 4000),
+  xlim = c(min(febr_data[, data_coleta_ano]),
+    as.integer(format(Sys.time(), "%Y"))),
+  main = ""
 )
 rug(febr_data[, data_coleta_ano])
 dev.off()
 
 # Criar objeto espacial e avaliar distribuição por bioma
-biomes <- geobr::read_biomes()[-7, ]
 febr_data_sf <- sf::st_as_sf(febr_data, coords = c("coord_x", "coord_y"), crs = 4623)
 dev.off()
 png("mapbiomas-solos/res/fig/carbon-stock-spatial-distribution.png",
   width = 480 * 3, height = 480 * 3, res = 72 * 3)
 plot(biomes["name_biome"], reset = FALSE,
-  main = "", axes = TRUE,
+  main = "", axes = TRUE, col = "transparent", lwd = 0.5, border = "darkgray",
   key.pos = NULL, graticule = TRUE)
-cex <- febr_data_sf[["carbono_estoque_g.m2"]] / (max(febr_data_sf[["carbono_estoque_g.m2"]]) * 0.3)
-plot(febr_data_sf["carbono_estoque_g.m2"], add = TRUE, pch = 20, cex = cex)
+cex <- febr_data_sf[["carbono_estoque_g.m2"]] / (max(febr_data_sf[["carbono_estoque_g.m2"]]) * 0.4)
+plot(febr_data_sf["carbono_estoque_g.m2"], add = TRUE, pch = 21, cex = cex, col = "black")
+prob <- c(0, 0.5, 0.95, 0.975, 1)
+legend(
+  x = -40, y = 8,
+  legend = paste0(round(quantile(febr_data_sf[["carbono_estoque_g.m2"]], prob) / 1000, 1), " kg/m^2"),
+  pch = 21, box.lwd = 0, pt.cex = quantile(cex, prob)
+)
 dev.off()
 
-# Escrever dados em disco
-febr_data[, espessura := NULL]
-data.table::fwrite(febr_data, "mapbiomas-solos/res/pontos-estoque.csv")
+# Write date to disk
+colnames(febr_data)
+data.table::fwrite(
+  febr_data,
+  paste0("mapbiomas-solos/res/tab/", format(Sys.time(), "%Y-%m-%d"), "-pontos-estoque-cos.csv")
+)
