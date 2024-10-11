e55cfa6f70d1fe709009432352031e23674e9d86
samuel-rosa <alessandrosamuelrosa@gmail.com>
2024-10-11 11:33:41 -0300

    create file to merge curated data

---
+++ src/13_merge_curated_data.R
@@ -0,0 +1,13 @@
+# title: SoilData - Soil Organic Carbon Stock
+# subtitle: Merge curated data
+# author: Alessandro Samuel-Rosa and Taciara Zborowski Horst
+# data: 2024 CC-BY
+rm(list = ls())
+
+# Install and load required packages
+if (!require("data.table")) {
+  install.packages("data.table")
+}
+
+# Source helper functions
+source("src/00_helper_functions.r")
