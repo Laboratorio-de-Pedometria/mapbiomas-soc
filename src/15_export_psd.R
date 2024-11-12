cdbea0eb1f8aa61f6517eb2e4cf96d3ed6c141e8
samuel-rosa <alessandrosamuelrosa@gmail.com>
2024-11-12 15:51:44 -0300

    compute PSD for fixed depths

--- src/15_export_psd.R
+++ src/15_export_psd.R
@@ -19,11 +19,63 @@ summary_soildata(soildata)
 # Events: 15499
 # Georeferenced events: 13151
 
-# We use natural splines to estimate the particle size distribution (PSD) at three target depths for
-# each event. The target depths are 0-10 cm, 10-20 cm, and 20-30 cm.
-clay_data <- soildata[, .(id, coord_x, coord_y, profund_inf, profund_sup, argila, silte, areia)]
-clay_data[, depth_mid := mean(c(profund_inf, profund_sup)), by = .I]
-clay_data[, clay0_10 := spline(x = depth_mid, y = argila, xout = 5, method = "natural")$y, by = id]
-clay_data[, clay10_20 := spline(x = depth_mid, y = argila, xout = 15, method = "natural")$y, by = id]
-clay_data[, clay20_30 := spline(x = depth_mid, y = argila, xout = 25, method = "natural")$y, by = id]
+# Create a data.table with the particle size distribution
+psd_data <- soildata[, .(id, coord_x, coord_y, profund_sup, profund_inf, argila, silte, areia)]
+psd_data[, depth_mid := profund_sup + (profund_inf - profund_sup) / 2, by = .I]
+psd_data[, has_psd := is.na(argila) + is.na(silte) + is.na(areia) == 0, by = id]
+psd_data[, has_coord := is.na(coord_x) + is.na(coord_y) == 0, by = id]
+print(psd_data)
+
+# Compute the clay content at 5, 15, and 25 cm depth
+psd_data <- psd_data[
+  has_psd == TRUE & has_coord == TRUE,
+  .(
+    coord_x = mean(coord_x, na.rm = TRUE),
+    coord_y = mean(coord_y, na.rm = TRUE),
+    clay0_10 = round(spline(x = depth_mid, y = argila, xout = 5, method = "natural")$y),
+    silt0_10 = round(spline(x = depth_mid, y = silte, xout = 5, method = "natural")$y),
+    sand0_10 = round(spline(x = depth_mid, y = areia, xout = 5, method = "natural")$y),
+    clay10_20 = round(spline(x = depth_mid, y = argila, xout = 15, method = "natural")$y),
+    silt10_20 = round(spline(x = depth_mid, y = silte, xout = 15, method = "natural")$y),
+    sand10_20 = round(spline(x = depth_mid, y = areia, xout = 15, method = "natural")$y),
+    clay20_30 = round(spline(x = depth_mid, y = argila, xout = 25, method = "natural")$y),
+    silt20_30 = round(spline(x = depth_mid, y = silte, xout = 25, method = "natural")$y),
+    sand20_30 = round(spline(x = depth_mid, y = areia, xout = 25, method = "natural")$y)
+  ),
+  by = id
+]
+
+soildata[id %in% psd_data[clay10_20 < 0, id]]
+
+# Check the sum of the particle size distribution
+psd_data[, psd0_10 := clay0_10 + silt0_10 + sand0_10]
+psd_data[, psd10_20 := clay10_20 + silt10_20 + sand10_20]
+psd_data[, psd20_30 := clay20_30 + silt20_30 + sand20_30]
+summary(psd_data[, .(psd0_10, psd10_20, psd20_30)])
+
+# Correct the particle size distribution to sum 100%
+# 0-10 cm depth
+psd_data[, clay0_10 := round(clay0_10 / psd0_10 * 100)]
+psd_data[, sand0_10 := round(sand0_10 / psd0_10 * 100)]
+psd_data[, silt0_10 := 100 - clay0_10 - sand0_10]
+
+# 10-20 cm depth
+psd_data[, clay10_20 := round(clay10_20 / psd10_20 * 100)]
+psd_data[, sand10_20 := round(sand10_20 / psd10_20 * 100)]
+psd_data[, silt10_20 := 100 - clay10_20 - sand10_20]
+
+# 20-30 cm depth
+psd_data[, clay20_30 := round(clay20_30 / psd20_30 * 100)]
+psd_data[, sand20_30 := round(sand20_30 / psd20_30 * 100)]
+psd_data[, silt20_30 := 100 - clay20_30 - sand20_30]
+
+
+
+summary(psd_data)
+
+# Plot using mapview
+if (FALSE) {
+  psd_data_sf <- sf::st_as_sf(psd_data, coords = c("coord_x", "coord_y"), crs = 4326)
+  mapview::mapview(psd_data_sf, zcol = "clay0_10")
+}
 
