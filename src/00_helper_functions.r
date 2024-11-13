30d8d9935c34fe9ac680df0532b854a9c8af2c21
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2024-11-13 15:31:07 -0300

    added code for texture classes

--- src/00_helper_functions.r
+++ src/00_helper_functions.r
@@ -216,3 +216,53 @@ google_sheet <- function(gs, gid) {
   dt <- data.table::fread(sheet_path, dec = ",", sep = "\t")
   return(dt)
 }
+
+# Compute the clay content at 5, 15, and 25 cm depth
+# psd_data <- psd_data[
+#   ,
+#   .(
+#     coord_x = mean(coord_x, na.rm = TRUE),
+#     coord_y = mean(coord_y, na.rm = TRUE),
+#     clay0_10 = round(spline(x = depth_mid, y = argila, xout = 5, method = "natural")$y),
+#     silt0_10 = round(spline(x = depth_mid, y = silte, xout = 5, method = "natural")$y),
+#     sand0_10 = round(spline(x = depth_mid, y = areia, xout = 5, method = "natural")$y),
+#     clay10_20 = round(spline(x = depth_mid, y = argila, xout = 15, method = "natural")$y),
+#     silt10_20 = round(spline(x = depth_mid, y = silte, xout = 15, method = "natural")$y),
+#     sand10_20 = round(spline(x = depth_mid, y = areia, xout = 15, method = "natural")$y),
+#     clay20_30 = round(spline(x = depth_mid, y = argila, xout = 25, method = "natural")$y),
+#     silt20_30 = round(spline(x = depth_mid, y = silte, xout = 25, method = "natural")$y),
+#     sand20_30 = round(spline(x = depth_mid, y = areia, xout = 25, method = "natural")$y)
+#   ),
+#   by = id
+# ]
+
+
+# Check the sum of the particle size distribution
+# psd_data[, psd0_10 := clay0_10 + silt0_10 + sand0_10]
+# psd_data[, psd10_20 := clay10_20 + silt10_20 + sand10_20]
+# psd_data[, psd20_30 := clay20_30 + silt20_30 + sand20_30]
+# summary(psd_data[, .(psd0_10, psd10_20, psd20_30)])
+
+# Correct the particle size distribution to sum 100%
+# 0-10 cm depth
+# psd_data[, clay0_10 := round(clay0_10 / psd0_10 * 100)]
+# psd_data[, sand0_10 := round(sand0_10 / psd0_10 * 100)]
+# psd_data[, silt0_10 := 100 - clay0_10 - sand0_10]
+
+# 10-20 cm depth
+# psd_data[, clay10_20 := round(clay10_20 / psd10_20 * 100)]
+# psd_data[, sand10_20 := round(sand10_20 / psd10_20 * 100)]
+# psd_data[, silt10_20 := 100 - clay10_20 - sand10_20]
+
+# 20-30 cm depth
+# psd_data[, clay20_30 := round(clay20_30 / psd20_30 * 100)]
+# psd_data[, sand20_30 := round(sand20_30 / psd20_30 * 100)]
+# psd_data[, silt20_30 := 100 - clay20_30 - sand20_30]
+
+# summary(psd_data)
+
+# Plot using mapview
+# if (FALSE) {
+#   psd_data_sf <- sf::st_as_sf(psd_data, coords = c("coord_x", "coord_y"), crs = 4326)
+#   mapview::mapview(psd_data_sf, zcol = "clay0_10")
+# }
