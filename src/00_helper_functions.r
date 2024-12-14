11dd3459a0ed809c37248bb774a2a8362f95f3ea
samuel-rosa <alessandrosamuelrosa@gmail.com>
2024-12-14 17:00:05 -0300

    minor

--- src/00_helper_functions.r
+++ src/00_helper_functions.r
@@ -2,7 +2,6 @@
 # subtitle: Helper functions
 # author: Alessandro Samuel-Rosa and Taciara Zborowski Horst
 # data: 2024 CC-BY
-rm(list = ls())
 
 # R function for the simple imputation of missing values in the columns of a data.frame.
 # The argument 'x' is a data.frame containing multiple columns. Columns of x can be of type continuous (numeric) or categorical (factor or character). Three simple imputation methods are available:
